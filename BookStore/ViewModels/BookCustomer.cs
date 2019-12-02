6951af56d0b595396cafc0b33d2cb6d51205ba14
Gosh <goshadzel@gmail.com>
2019-12-02 03:38:22 +0200

    created a new viewmodel that contains User and Book models combined

---
+++ BookStore/ViewModels/BookCustomer.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.ViewModels
+{
+    public class BookCustomer
+    {
+       public User User { get; set; }
+       public Book Book { get; set; }
+    }
+}
